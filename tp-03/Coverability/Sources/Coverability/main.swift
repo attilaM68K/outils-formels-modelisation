0e9b9472d6bc59fc5c6e2787372961d0a3da023b
AttilaM68K <Attila.Nacsa@etu.unige.ch>
2017-11-09 23:55:28 +0100

    TP3 finished

--- tp-03/Coverability/Sources/Coverability/main.swift
+++ tp-03/Coverability/Sources/Coverability/main.swift
@@ -1,6 +1,26 @@
 import PetriKit
 import CoverabilityLib
 
+var model = createBoundedModel()
+// Retrieve places model.
+guard let r  = model.places.first(where: { $0.name == "r" }),
+      let p  = model.places.first(where: { $0.name == "p" }),
+      let t  = model.places.first(where: { $0.name == "t" }),
+      let m  = model.places.first(where: { $0.name == "m" }),
+      let w1 = model.places.first(where: { $0.name == "w1" }),
+      let s1 = model.places.first(where: { $0.name == "s1" }),
+      let w2 = model.places.first(where: { $0.name == "w2" }),
+      let s2 = model.places.first(where: { $0.name == "s2" }),
+      let w3 = model.places.first(where: { $0.name == "w3" }),
+      let s3 = model.places.first(where: { $0.name == "s3" })
+else {
+    fatalError("invalid model")
+}
+
+let initialMarking: CoverabilityMarking = [r: 1, p: 0, t: .omega, m: 0, w1: 1, s1: 0, w2: 1, s2: 0, w3: 1, s3: 0]
+// let graph = model.coverabilityGraph(from: initialMarking)
+// Did not have time to correct an infinite loop error
+
 // This file contains the code that will be executed if you run your program from the terminal. You
 // don't have to write anything in this file, but you may use it to debug your code. You can create
 // instances of the provided models as the following:
@@ -8,7 +28,7 @@ import CoverabilityLib
 //     let model          = createBoundedModel()
 //     let unboundedModel = createUnboundedModel()
 //
-// Or you can create any instance of your own models to test your code.
+// Or you can creates any instance of your own models to test your code.
 //
 // You **are** encouraged to write tests of your own!
 // You may write as many tests as you want here, or even better in `CoverabilityLibTests.swift`.
--- tp-03/Coverability/Sources/CoverabilityLib/PTNet+Extensions.swift
+++ tp-03/Coverability/Sources/CoverabilityLib/PTNet+Extensions.swift
@@ -5,6 +5,49 @@ public extension PTNet {
     public func coverabilityGraph(from marking: CoverabilityMarking) -> CoverabilityGraph {
         // Write here the implementation of the coverability graph generation.
 
+
+        let array_transitions = Array(transitions) // Put all transitions in an array for easier testing
+        var mark_init = CoverabilityGraph(marking: marking, successors: [:]) // initial marking
+        var mark_todo = [mark_init] // Markings to do
+        var mark_visited = [mark_init] // Markings already visited
+        var curr_mark = mark_init // The marking that is currently tested
+        var curr_trans: CoverabilityMarking?
+        var mark_next = mark_init // The resultant marking from curr_trans
+        var if_end = true // For a single piece of code in the while loop, which should only be executed once
+
+        while !(mark_todo.isEmpty) {
+          curr_mark = mark_todo[0] // Test the first element for each iteration
+          for transit in self.transitions {
+            curr_trans = transit.fireCover(from: curr_mark.marking) // Try the transition
+            if (nil != curr_trans) { // If curr_trans is not nill then...
+              if (curr_trans! > mark_init.marking) { // If the marking is bigger then...
+                for place_check in self.places { // Go through each PTPlace
+                  if curr_trans![place_check]! > mark_init.marking[place_check]! { // If there's a bigger place then we replace it with omega
+                    curr_trans![place_check] = .omega
+                  }
+                }
+              }
+              mark_next = CoverabilityGraph(marking: curr_trans!, successors: [:]) // ...store the resulting marking
+              if !(mark_visited.contains(where: { $0.marking == mark_next.marking})) { // if the marking is not visited yet
+                curr_mark.successors.updateValue(mark_next, forKey: transit) // add marking as successor
+                mark_todo.append(mark_next) // Add marking to the waiting list
+                mark_visited.append(mark_next) // Add marking to the list of visited ones
+              } else { // Otherwise...
+                curr_mark.successors.updateValue(mark_next, forKey: transit) // ...add marking as successor
+              }
+            }
+          }
+          if (if_end) { // Only run this part of the code ONCE
+            if_end = false
+            mark_init = curr_mark // Modify the mark_init to contain the successors
+          }
+          mark_todo.remove(at: 0) // all tests finished for curr_mark, remove it from the waiting list
+        }
+        return mark_init // The initial marking is ideal as return value because
+        // every marking found during the execution of the function is visible
+        // from here
+
+
         // Note that CoverabilityMarking implements both `==` and `>` operators, meaning that you
         // may write `M > N` (with M and N instances of CoverabilityMarking) to check whether `M`
         // is a greater marking than `N`.
@@ -13,7 +56,43 @@ public extension PTNet {
         // print debug information you'll write in that function will NOT be taken into account to
         // evaluate your homework.
 
-        return CoverabilityGraph(marking: marking)
     }
+}
 
+public extension PTTransition {
+  public func isFireableCover(from marking: CoverabilityMarking) -> Bool {
+      for arc in self.preconditions {
+          if marking[arc.place]! < Token.some(arc.tokens) {
+              return false
+          }
+      }
+
+      return true
+  }
+
+  public func fireCover(from marking: CoverabilityMarking) -> CoverabilityMarking? {
+      guard self.isFireableCover(from: marking) else {
+          return nil
+      }
+
+      var result = marking
+      for arc in self.preconditions {
+        switch result[arc.place] {
+        case .some(.some(let x)):
+          result[arc.place]! = .some(x-arc.tokens)
+        default:
+          break
+        }
+      }
+      for arc in self.postconditions {
+        switch (result[arc.place]){
+        case .some(.some(let x)):
+          result[arc.place]! = .some(x+arc.tokens)
+        default:
+          break
+        }
+      }
+
+      return result
+  }
 }
